7353a3a1e6e5fb48f547998f78e17e8c4a10bc7a
Shtya <Shtya54@gmail.com>
2023-12-09 13:20:30 +0200

    21

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { hydrate, render } from "react-dom";
 import App from "./App"
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
@@ -7,16 +8,15 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import {BrowserRouter} from "react-router-dom"
 import { Helmet } from 'react-helmet';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Helmet>
-      <title>MRM</title>
-      
-      <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." />
-    </Helmet>
+const root = document.getElementById("root");
 
-    <App />
-  </BrowserRouter>
-);
+if (root.hasChildNodes() ) {
+  hydrate(<BrowserRouter> <Helmet>   <title>MRM</title>   <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." /> </Helmet> <App /> </BrowserRouter>, root);
+} else {
+  render(<BrowserRouter> <Helmet>   <title>MRM</title>   <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." /> </Helmet> <App /> </BrowserRouter>, root);
+}
+
+
+
+
 
