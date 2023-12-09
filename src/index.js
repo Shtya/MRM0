0cc099f370cd4bd332e358f0fd0168582b9286ea
Shtya <Shtya54@gmail.com>
2023-12-09 11:41:54 +0200

    21

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/js/bootstrap.min.js'
 import {BrowserRouter} from "react-router-dom"
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <Helmet>
+      <title>MRM</title>
+      <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." />
+    </Helmet>
+
     <App />
   </BrowserRouter>
 );
