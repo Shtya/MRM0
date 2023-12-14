76e41c603fedf831c90724056cf01f4b401fdf11
Shtya <Shtya54@gmail.com>
2023-12-14 09:02:19 +0200

    34

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { hydrate, render } from "react-dom";
 import App from "./App"
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/js/bootstrap.min.js'
 import {BrowserRouter} from "react-router-dom"
 import { Helmet } from 'react-helmet';
 
 const root = document.getElementById("root");
 
-if (root.hasChildNodes() ) {
-  hydrate(<BrowserRouter> <Helmet>   <title>MRM</title>   <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." /> </Helmet> <App /> </BrowserRouter>, root);
-} else {
-  render(<BrowserRouter> <Helmet>   <title>MRM</title>   <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." /> </Helmet> <App /> </BrowserRouter>, root);
-}
+ReactDOM.render(
+  <BrowserRouter> 
+    <Helmet>   <title>MRM</title>   <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." /> </Helmet> 
+    <App /> 
+  </BrowserRouter>, root)
+
+// if (root.hasChildNodes() ) {
+//   hydrate(<BrowserRouter> <Helmet>   <title>MRM</title>   <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." /> </Helmet> <App /> </BrowserRouter>, root);
+// } else {
+//   render(<BrowserRouter> <Helmet>   <title>MRM</title>   <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." /> </Helmet> <App /> </BrowserRouter>, root);
+// }
 
 
 
