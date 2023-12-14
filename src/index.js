4ea503fd670d374a078f56dcc69358e8d9e2b780
Shtya <Shtya54@gmail.com>
2023-12-14 09:12:21 +0200

    34

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ ReactDOM.render(
     <App /> 
   </BrowserRouter>, root)
 
+
 // if (root.hasChildNodes() ) {
 //   hydrate(<BrowserRouter> <Helmet>   <title>MRM</title>   <meta name="description" content="MRM is the premium solution provider for all type of printing & advertising in the uae. We are proud to produce some of the finest work in that sector. We are creative agency, providing customers with cross-media solutions, graphic and online design, offset, digital and large format printing as well as marketing promotions." /> </Helmet> <App /> </BrowserRouter>, root);
 // } else {
